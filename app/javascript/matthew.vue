a387038785d1ed6eba773d62e75066a3b2858f19
Matthew Hargraves <matthew.hargraves@cef.co.uk>
2020-02-09 13:24:21 +0000

    add new player

--- app/javascript/matthew.vue
+++ app/javascript/matthew.vue
@@ -1,57 +1,63 @@
 <template>
   <div>
+    <label> Add new player
+      <input name="newPlayer" id="newPlayer" placeholder="first_name surname">
+      <button v-on:click="addPlayer">Submit</button>
+    </label>
+
+
     <label>Player 1
       <select v-model="team_sheet.player_1">
-        <option v-for="player in players" v-bind:value="player.first_name + ' ' + player.surname">
+        <option v-for="player in club_players" v-bind:value="player.first_name + ' ' + player.surname">
           {{ player.first_name + " " + player.surname }}
         </option>
       </select>
     </label>
     <label>Player 2
       <select v-model="team_sheet.player_2">
-        <option v-for="player in players" v-bind:value="player.first_name + ' ' + player.surname">
+        <option v-for="player in club_players" v-bind:value="player.first_name + ' ' + player.surname">
           {{ player.first_name + " " + player.surname }}
         </option>
       </select>
     </label>
     <label>Player 3
       <select v-model="team_sheet.player_3">
-        <option v-for="player in players" v-bind:value="player.first_name + ' ' + player.surname">
+        <option v-for="player in club_players" v-bind:value="player.first_name + ' ' + player.surname">
           {{ player.first_name + " " + player.surname }}
         </option>
       </select>
     </label>
     <label>Player 4
       <select v-model="team_sheet.player_4">
-        <option v-for="player in players" v-bind:value="player.first_name + ' ' + player.surname">
+        <option v-for="player in club_players" v-bind:value="player.first_name + ' ' + player.surname">
           {{ player.first_name + " " + player.surname }}
         </option>
       </select>
     </label>
     <label>Player 5
       <select v-model="team_sheet.player_5">
-        <option v-for="player in players" v-bind:value="player.first_name + ' ' + player.surname">
+        <option v-for="player in club_players" v-bind:value="player.first_name + ' ' + player.surname">
           {{ player.first_name + " " + player.surname }}
         </option>
       </select>
     </label>
     <label>Player 6
       <select v-model="team_sheet.player_6">
-        <option v-for="player in players" v-bind:value="player.first_name + ' ' + player.surname">
+        <option v-for="player in club_players" v-bind:value="player.first_name + ' ' + player.surname">
           {{ player.first_name + " " + player.surname }}
         </option>
       </select>
     </label>
     <label>Player 7
       <select v-model="team_sheet.player_7">
-        <option v-for="player in players" v-bind:value="player.first_name + ' ' + player.surname">
+        <option v-for="player in club_players" v-bind:value="player.first_name + ' ' + player.surname">
           {{ player.first_name + " " + player.surname }}
         </option>
       </select>
     </label>
     <label>Player 8
       <select v-model="team_sheet.player_8">
-        <option v-for="player in players" v-bind:value="player.first_name + ' ' + player.surname">
+        <option v-for="player in club_players" v-bind:value="player.first_name + ' ' + player.surname">
           {{ player.first_name + " " + player.surname }}
         </option>
       </select>
@@ -87,14 +93,20 @@
     props: ['players', 'team_sheet', 'club', 'fixture'],
 
     mounted() {
-      console.log(this.club)
+      console.log(this.club);
     },
-
+    data() {
+      return {
+        club_players: this.players
+      }
+    },
     methods: {
       addPlayer: function() {
-        this.team.players.push({
-          first_name: "",
-          surname: ""
+        let newPlayerEl = document.getElementById("newPlayer");
+        let name = newPlayerEl.value.split(" ");
+        this.club_players.push({
+          first_name: name[0],
+          surname: name[1]
         })
       },
       submit: function() {
