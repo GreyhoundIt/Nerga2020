2e8b60bbe7b945ae3525f57592653c5d4c482dca
Matthew Hargraves <matthew.hargraves@cef.co.uk>
2020-05-21 14:54:32 +0100

    ie partial fix and add reek

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -7,3 +7,24 @@ window.onload= function (){
         console.log("Clicked");
     })
 }
+
+
+function GetIEVersion() {
+    var sAgent = window.navigator.userAgent;
+    var Idx = sAgent.indexOf("MSIE");
+
+    // If IE, return version number.
+    if (Idx > 0)
+        return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+    // If IE 11 then look for Updated user agent string.
+    else if (!!navigator.userAgent.match(/Trident\/7\./))
+        return 11;
+
+    else
+        return 0; //It is not IE
+}
+window.onload= function () {
+    if (GetIEVersion() > 0)
+        alert("This website is best viewed on a newer browser. Please use Edge/Chrome/Firefox");
+}
